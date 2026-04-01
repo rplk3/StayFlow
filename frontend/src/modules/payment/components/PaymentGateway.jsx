37917a7dfaf8d92e4967bc2c2ff66e696f589f49
IT23320482 <it23320482@my.sliit.lk>
2026-04-01 19:15:09 +0100

    Update PaymentGateway.jsx

--- frontend/src/modules/payment/components/PaymentGateway.jsx
+++ frontend/src/modules/payment/components/PaymentGateway.jsx
@@ -1,1 +1,4 @@
-
+import React, { useState } from 'react';
+import { CreditCard, Lock, CheckCircle, XCircle, Loader2, AlertCircle, ShieldCheck, Calendar, Hash, Download, Mail, UserPlus } from 'lucide-react';
+import axios from 'axios';
+import jsPDF from 'jspdf';
